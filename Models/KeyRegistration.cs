ae15180669624dacddb58039ad9cb36379f423d9
marcosin78 <marcbravomadrid@gmail.com>
2026-02-09 22:22:12 +0100

    Primera version

--- Models/KeyRegistration.cs
+++ Models/KeyRegistration.cs
@@ -1,55 +1,122 @@
 using System;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace MonitorInputApp.Models
 {
-	public class KeyRegistration
-	{
-		private DateTime _lastKeyPressTime;
-		private bool _isRunning;
-		private CancellationTokenSource? _cts;
+    public class KeyRegistration
+    {
+        private IntPtr _keyboardHookID = IntPtr.Zero;
+        private IntPtr _mouseHookID = IntPtr.Zero;
+        private LowLevelKeyboardProc _keyboardProc;
+        private LowLevelMouseProc _mouseProc;
+        private DateTime _lastInputTime;
+        private System.Timers.Timer? _timer;
 
-		public KeyRegistration()
-		{
-			_lastKeyPressTime = DateTime.MinValue;
-			_isRunning = false;
-		}
+        public KeyRegistration()
+        {
+            _keyboardProc = KeyboardHookCallback;
+            _mouseProc = MouseHookCallback;
+            _lastInputTime = DateTime.MinValue;
+        }
 
-		public void Start()
-		{
-			if (_isRunning) return;
-			_isRunning = true;
-			_cts = new CancellationTokenSource();
-			Task.Run(() => MonitorInput(_cts.Token));
-		}
+        public void Start()
+        {
+            _keyboardHookID = SetKeyboardHook(_keyboardProc);
+            _mouseHookID = SetMouseHook(_mouseProc);
 
-		public void Stop()
-		{
-			_isRunning = false;
-			_cts?.Cancel();
-		}
+            _timer = new System.Timers.Timer(5000);
+            _timer.Elapsed += (s, e) =>
+            {
+                if (_lastInputTime != DateTime.MinValue)
+                {
+                    Console.WriteLine($"Última entrada detectada a las {_lastInputTime:HH:mm:ss}");
+                }
+            };
+            _timer.Start();
+        }
 
-		private async Task MonitorInput(CancellationToken token)
-		{
-			while (!token.IsCancellationRequested)
-			{
-				if (Console.KeyAvailable)
-				{
-					var key = Console.ReadKey(true);
-					_lastKeyPressTime = DateTime.Now;
-					Console.WriteLine($"Tecla presionada: {key.KeyChar} a las {_lastKeyPressTime:HH:mm:ss}");
-				}
-				if (_lastKeyPressTime != DateTime.MinValue)
-				{
-					Console.WriteLine($"Última entrada detectada a las {_lastKeyPressTime:HH:mm:ss}");
-					await Task.Delay(5000, token);
-				}
-				else
-				{
-					await Task.Delay(100, token);
-				}
-			}
-		}
-	}
+        public void Stop()
+        {
+            UnhookWindowsHookEx(_keyboardHookID);
+            UnhookWindowsHookEx(_mouseHookID);
+            _timer?.Stop();
+            _timer?.Dispose();
+            _timer = null;
+        }
+
+        private IntPtr SetKeyboardHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule!)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr SetMouseHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule!)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int WM_KEYDOWN = 0x0100;
+                if (wParam == (IntPtr)WM_KEYDOWN)
+                {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    _lastInputTime = DateTime.Now;
+                    Console.WriteLine($"Tecla presionada: {vkCode} a las {_lastInputTime:HH:mm:ss}");
+                }
+            }
+            return CallNextHookEx(_keyboardHookID, nCode, wParam, lParam);
+        }
+
+        private IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int WM_LBUTTONDOWN = 0x0201;
+                int WM_RBUTTONDOWN = 0x0204;
+                if (wParam == (IntPtr)WM_LBUTTONDOWN)
+                {
+                    _lastInputTime = DateTime.Now;
+                    Console.WriteLine($"Botón izquierdo del ratón presionado a las {_lastInputTime:HH:mm:ss}");
+                }
+                else if (wParam == (IntPtr)WM_RBUTTONDOWN)
+                {
+                    _lastInputTime = DateTime.Now;
+                    Console.WriteLine($"Botón derecho del ratón presionado a las {_lastInputTime:HH:mm:ss}");
+                }
+            }
+            return CallNextHookEx(_mouseHookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WH_MOUSE_LL = 14;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, Delegate lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
 }
--- obj/Debug/net10.0/MonitorInputApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MonitorInputApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonitorInputApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48d1a14a8e6ec27f205a1dc62252a5ca4b86ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonitorInputApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonitorInputApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
